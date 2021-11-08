45a140690822553ab4cda6ff33ddd2cd279faba7
smediums <smediums.me@gmail.com>
2021-11-08 20:23:38 +0300

    Javascript timer. Select mintues and seconds and run timer.

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
-const btn = document.getElementById('changeClr')
-const hex = document.getElementById('hexCode')
-const body = document.querySelector('body')
+const time = document.getElementById('time')
+const setTime = document.getElementById('setTime')
+const start = document.getElementById('start')
+const minutes = document.querySelectorAll('.minute')
+const seconds = document.querySelectorAll('.second')
+const secSelector = document.getElementById('theSeconds')
+const minSelector = document.getElementById('minutes')
+let secondNum;
 
-btn.addEventListener('click', () => {
-    let color = '#';
-    color += Math.random().toString(16).slice(2,8)
-    body.style.background = color
-    hex.innerText = color;
+
+//Give select options a value
+let initMin = -1
+let initSec = -1
+minutes.forEach(minute => {
+    initMin++;
+    minute.innerText = initMin;
 })
+seconds.forEach(second => {
+    initSec++;
+    second.innerText = initSec;
+})
+
+
+
+
+
+//Timer functionality
+const runTimer = () => {
+    //set input value as initial timer value
+    secondNum = parseInt(secSelector.value) + (parseInt(minSelector.value) * 60);
+
+    //If no input value
+    if(secondNum == ''){
+        alert('Please enter valid time')
+    }else{
+
+    //Run timer
+    const countDown = setInterval(() => {
+    secondNum--;
+        showTime(secondNum)
+        if(secondNum <= 0){
+            clearInterval(countDown);
+        }
+        }, 1000);
+        const showTime = (second) => {
+            const min = Math.floor(second / 60)
+            const sec = Math.floor(second % 60)
+
+            time.innerHTML =`${min < 10 ? '0': ''}${min}:${sec < 10 ? '0': ''}${sec}`
+        }
+    }
+}
+
+//start timer
+start.addEventListener('click', runTimer)
